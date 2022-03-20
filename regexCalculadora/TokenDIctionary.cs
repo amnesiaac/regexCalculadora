9fbe852730c2677df116971b72ba8b53648f126c
amnesiaac <bruno.felipe130@gmail.com>
2022-03-20 20:11:11 -0300

    adicionando regex

--- regexCalculadora/TokenDIctionary.cs
+++ regexCalculadora/TokenDIctionary.cs
@@ -4,7 +4,13 @@ namespace regexCalculadora
 {
     public class TokenDIctionary
     {
-        List<TokenDefinition> _tokenDefinitions;
+        List<TokenDefinition> _tokenDefinitions = new List<TokenDefinition>();
+        public TokenDIctionary()
+        {
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.Number, @"^\d$"));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.Operator, @"[*+\/-]"));
+            _tokenDefinitions.Add(new TokenDefinition(TokenType.Word, "[a-zA-Z]"));
+        }
 
     }
 }
